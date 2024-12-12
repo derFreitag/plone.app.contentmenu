e47baa5cd9d2bce5eddb97fa338a9aa140bc6ab5
Gil Forcada Codinachs <gil.gnome@gmail.com>
2024-12-12 12:55:59 +0100

    Back to development: 3.0.6.post1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-version = "3.0.6.post0"
+version = "3.0.6.post1.dev0"
 long_description = open("README.rst").read() + "\n"
 long_description += open("CHANGES.rst").read()
 
